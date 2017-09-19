429157ea1f9e233c415201f91e368e7a5bb22367
Cai Tengyuan <caitengyuan@baidu.com>
2017-09-19 10:14:06 +0800

    Initial Commit

---
+++ XCWKWebViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCWKWebViewDemo
+//
+//  Created by Cai,Tengyuan on 2017/9/19.
+//  Copyright © 2017年 Cai,Tengyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XCWKWebViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCWKWebViewDemo
+//
+//  Created by Cai,Tengyuan on 2017/9/19.
+//  Copyright © 2017年 Cai,Tengyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
